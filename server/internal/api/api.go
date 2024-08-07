4f466663c600674e01ea9482541c0525c7509917
Guilherme Toledo <totoledao@hotmail.com>
2024-08-07 15:31:47 -0300

    Create routes and create room request

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"log/slog"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+	"github.com/jackc/pgx/v5"
 	"github.com/totoledao/ask-anything-chat/internal/store/pgstore"
 )
 
@@ -33,6 +35,26 @@ func NewHandler(q *pgstore.Queries, r *chi.Mux, upgrader websocket.Upgrader) htt
 	}
 
 	r.Get("/subscribe/{room_id}", a.handleSubscribeToRoom)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/rooms", func(r chi.Router) {
+			r.Post("/", a.handlePostCreateRoom)
+			r.Get("/", a.handleGetRooms)
+
+			r.Route("/{room_id}/messages", func(r chi.Router) {
+				r.Post("/", a.handlePostCreateRoomMessages)
+				r.Get("/", a.handleGetRoomMessages)
+
+				r.Route("/{message_id}", func(r chi.Router) {
+					r.Get("/", a.handleGetRoomMessage)
+					r.Patch("/", a.handlePatchReactToMessage)
+					r.Patch("/", a.handlePatchMarkMessageAsAnswered)
+					r.Delete("/", a.handleDeleteReactionFromMessage)
+				})
+
+			})
+		})
+	})
 
 	a.r = r
 	return a
@@ -82,4 +104,64 @@ func (h apiHandler) handleSubscribeToRoom(w http.ResponseWriter, r *http.Request
 	h.mu.Lock()
 	delete(h.subscribers[rawRoomID], c)
 	h.mu.Unlock()
+}
+
+func (h apiHandler) handlePostCreateRoom(w http.ResponseWriter, r *http.Request) {
+	type _body struct {
+		Theme string `json:"theme"`
+	}
+
+	var body _body
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "Invalid json", http.StatusBadRequest)
+		return
+	}
+
+	roomID, err := h.q.InsertRoom(r.Context(), body.Theme)
+	if err != nil {
+		slog.Error("Failed to insert Room", "error", err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	type response struct {
+		ID string `json:"id"`
+	}
+
+	data, err := json.Marshal(response{ID: roomID.String()})
+	if err != nil {
+		slog.Error("Failed to parse Response", "error", err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	_, err = w.Write(data)
+	if err != nil {
+		slog.Error("Failed to write Response", "error", err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+}
+func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h apiHandler) handlePostCreateRoomMessages(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h apiHandler) handleGetRoomMessages(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h apiHandler) handlePatchReactToMessage(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h apiHandler) handlePatchMarkMessageAsAnswered(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h apiHandler) handleDeleteReactionFromMessage(w http.ResponseWriter, r *http.Request) {
+
 }
